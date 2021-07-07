c8272221d46ae7808020747e91c1455fd852c9a4
alexazarh <aazarh@gmail.com>
2021-07-07 17:35:08 -0500

    added config to TerraformShell to enable/disable live status updates and writing messages to sandbox output

---
+++ package/cloudshell/iac/terraform/services/live_status_updater.py
@@ -0,0 +1,17 @@
+from cloudshell.api.cloudshell_api import CloudShellAPISession
+
+
+class LiveStatusUpdater:
+    def __init__(self, api: CloudShellAPISession, sandbox_id: str, update_live_status):
+        self._api = api
+        self._sandbox_id = sandbox_id
+        self._update_live_status = update_live_status
+
+    def set_service_live_status(self, service_name: str, status: str, description: str):
+        if self._update_live_status:
+            self._api.SetServiceLiveStatus(
+                self._sandbox_id,
+                service_name,
+                status,
+                description
+            )
--- package/cloudshell/iac/terraform/terraform_shell.py
+++ package/cloudshell/iac/terraform/terraform_shell.py
@@ -5,36 +5,41 @@ from cloudshell.shell.core.driver_context import ResourceCommandContext
 from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
 from cloudshell.shell.core.session.logging_session import LoggingSessionContext
 
+from cloudshell.iac.terraform import TerraformShellConfig
 from cloudshell.iac.terraform.downloaders.downloader import Downloader
 from cloudshell.iac.terraform.models.shell_helper import ShellHelperObject
 from cloudshell.iac.terraform.services.input_output_service import InputOutputService
+from cloudshell.iac.terraform.services.live_status_updater import LiveStatusUpdater
 from cloudshell.iac.terraform.services.provider_handler import ProviderHandler
+from cloudshell.iac.terraform.services.sandbox_messages import SandboxMessagesService
 from cloudshell.iac.terraform.services.sandox_data import SandboxDataHandler
 from cloudshell.iac.terraform.services.tf_proc_exec import TfProcExec
 
 
 class TerraformShell:
     # todo: add support to provide the info needed from attributes as parameters to the init (not shell attributes)
-    def __init__(self, driver_context: ResourceCommandContext, terraform_service_shell: any, logger: logging.Logger):
+    def __init__(self, driver_context: ResourceCommandContext, terraform_service_shell: any,
+                 logger: logging.Logger = None, config: TerraformShellConfig = None):
         self._context = driver_context
         self._tf_service = terraform_service_shell
         self._logger = logger
+        self._config = config or TerraformShellConfig()
 
     def execute_terraform(self):
         # initialize a logger if logger wasn't passed during init
         with nullcontext(self._logger) if self._logger else LoggingSessionContext(self._context) as logger:
 
-            shell_model = self._create_shell_helper(logger)
+            shell_helper = self._create_shell_helper(logger)
 
-            downloader = Downloader(shell_model)
+            downloader = Downloader(shell_helper)
             tf_workingdir = downloader.download_terraform_module()
             downloader.download_terraform_executable(tf_workingdir)
 
-            tf_proc_executer = TfProcExec(shell_model,
-                                          SandboxDataHandler(shell_model, tf_workingdir),
-                                          InputOutputService(shell_model))
+            tf_proc_executer = TfProcExec(shell_helper,
+                                          SandboxDataHandler(shell_helper, tf_workingdir),
+                                          InputOutputService(shell_helper))
             if tf_proc_executer.can_execute_run():
-                ProviderHandler.initialize_provider(shell_model)
+                ProviderHandler.initialize_provider(shell_helper)
                 tf_proc_executer.init_terraform()
                 tf_proc_executer.plan_terraform()
                 tf_proc_executer.apply_terraform()
@@ -42,7 +47,7 @@ class TerraformShell:
             else:
                 err_msg = "Execution is not enabled due to either failed previous Execution (*Try Destroy first) or " \
                           "Successfully executed previously without successfully destroying it first"
-                shell_model.api.WriteMessageToReservationOutput(shell_model.sandbox_id, err_msg)
+                shell_helper.sandbox_messages.write_message(err_msg)
                 raise Exception(err_msg)
 
     def destroy_terraform(self):
@@ -65,4 +70,9 @@ class TerraformShell:
     def _create_shell_helper(self, logger: logging.Logger) -> ShellHelperObject:
         api = CloudShellSessionContext(self._context).get_api()
         sandbox_id = self._context.reservation.reservation_id
-        return ShellHelperObject(api, sandbox_id, self._tf_service, logger)
+        sandbox_message_service = SandboxMessagesService(api, sandbox_id, self._tf_service.name,
+                                                         self._config.write_sandbox_messages)
+        live_status_updater = LiveStatusUpdater(api, sandbox_id, self._config.update_live_status)
+
+        return ShellHelperObject(api, sandbox_id, self._tf_service, logger, sandbox_message_service,
+                                 live_status_updater)
