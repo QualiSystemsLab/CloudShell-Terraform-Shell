bc52bbf5f622a4f5cc2edcec4bf6b1f9b5d63c1e
D4N <dan.amzulescu@gmail.com>
2021-07-12 17:24:51 -0500

    fixed integration testing

--- package/tests/constants.py
+++ package/tests/constants.py
@@ -1,7 +1,7 @@
-GITHUB_TF_PUBLIC_HELLO_URL_FILE = "https://github.com/QualiSystemsLab/CloudShell-Terraform-Shell/blob/master/tests/tests_helper_files/tf_modules/hello/hello.tf"
-GITHUB_TF_PUBLIC_HELLO_URL_FOLDER = "https://github.com/QualiSystemsLab/CloudShell-Terraform-Shell/tree/master/tests/tests_helper_files/tf_modules/hello"
-
+GITHUB_TF_PUBLIC_HELLO_URL_FILE = "https://github.com/QualiSystemsLab/CloudShell-Terraform-Shell/blob/master/package/tests/tests_helper_files/tf_modules/hello/hello.tf"
+GITHUB_TF_PUBLIC_HELLO_URL_FOLDER = "https://github.com/QualiSystemsLab/CloudShell-Terraform-Shell/tree/master/package/tests/tests_helper_files/tf_modules/hello"
 
 TERRAFORM_EXEC_FILE = "terraform.exe"
 TF_HELLO_FILE = "hello.tf"
+SHELL_NAME = "Generic Terraform Service"
 SHELL_NAME = "Generic Terraform Service"
--- package/tests/integration_tests/int_test_tf_execute_destroy.py
+++ package/tests/integration_tests/int_test_tf_execute_destroy.py
@@ -11,13 +11,13 @@ class TestTerraformExecuteDestroy(TestCase):
 
     def test_execute_and_destroy(self):
         self.integration_data.context.resource.attributes[f"{SHELL_NAME}.Terraform Inputs"] = \
-            os.environ.get(" VAULT_TF_INPUTS")
+            os.environ.get("AZUREAPP_TF_INPUTS")
         self.integration_data.context.resource.attributes[
             f"{SHELL_NAME}.Github Terraform Module URL"] = os.environ.get("GITHUB_TF_PRIVATE_AZUREAPP_URL")
         self.integration_data.context.resource.attributes[
             f"{SHELL_NAME}.UUID"] = ""
 
-        self.integration_data.context.real_api.ClearSandboxData(self.integration_data._driver_helper.sandbox_id)
+        self.integration_data.real_api.ClearSandboxData(self.integration_data.driver_helper.sandbox_id)
 
         self.integration_data.driver.execute_terraform(self.integration_data.context)
 
@@ -25,9 +25,8 @@ class TestTerraformExecuteDestroy(TestCase):
         # And also replace the custom inputs and TF URL
         self.integration_data.set_context_resource_attributes()
         self.integration_data.context.resource.attributes[f"{SHELL_NAME}.Terraform Inputs"] = \
-            os.environ.get(" VAULT_TF_INPUTS")
+            os.environ.get("AZUREAPP_TF_INPUTS")
         self.integration_data.context.resource.attributes[
             f"{SHELL_NAME}.Github Terraform Module URL"] = os.environ.get("GITHUB_TF_PRIVATE_VAULT_URL")
 
         self.integration_data.driver.destroy_terraform(self.integration_data.context)
-
