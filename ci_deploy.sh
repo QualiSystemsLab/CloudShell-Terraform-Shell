05c70c890448608c067722a5f5c27af3115e17be
D4N <dan.amzulescu@gmail.com>
2021-08-04 12:50:23 -0500

    :neutral_face:

--- ci_deploy.sh
+++ ci_deploy.sh
@@ -4,5 +4,5 @@ cd package
 python setup.py sdist
 cd dist
 package_name=$(find ./ -maxdepth 1 -type f -name "cloudshell-iac-terraform-*.tar.gz" -printf "%f\n")
-pip install $package_names
+pip install $package_name
 cd ..
