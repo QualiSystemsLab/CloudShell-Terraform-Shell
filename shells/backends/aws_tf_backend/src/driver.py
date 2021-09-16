406b1446aa88442c3dbeccbe50ef1fdb857a921d
qualidan <dan.amzulescu@gmail.com>
2021-09-16 11:30:38 -0500

    flake8 fixses

--- shells/backends/aws_tf_backend/src/driver.py
+++ shells/backends/aws_tf_backend/src/driver.py
@@ -2,8 +2,7 @@ import json
 
 from botocore.exceptions import ClientError
 from cloudshell.shell.core.resource_driver_interface import ResourceDriverInterface
-from cloudshell.shell.core.driver_context import InitCommandContext, ResourceCommandContext, AutoLoadDetails, \
-    CancellationContext
+from cloudshell.shell.core.driver_context import AutoLoadDetails
 # from data_model import *  # run 'shellfoundry generate' to generate data model classes
 from cloudshell.shell.core.session.cloudshell_session import CloudShellSessionContext
 from cloudshell.shell.core.session.logging_session import LoggingSessionContext
@@ -11,7 +10,7 @@ import boto3
 
 from aws_tf_backend.src.constants import AWS_MODELS, AWS2G_MODEL
 from azure_tf_backend.src.constants import ACCESS_KEY_ATTRIBUTE
-from data_model import *
+from data_model import AwsTfBackend
 
 
 class AwsTfBackendDriver (ResourceDriverInterface):
@@ -204,85 +203,4 @@ class AwsTfBackendDriver (ResourceDriverInterface):
     def _handle_exception_logging(self, logger, msg):
         logger.exception(msg)
         raise ValueError(msg)
-    # </editor-fold>
-
-    # <editor-fold desc="orchestration save/restore">
-    def orchestration_save(self, context, cancellation_context, mode, custom_params):
-        """
-        Saves the Shell state and returns a description of the saved artifacts and information
-        This command is intended for API use only by sandbox orchestration scripts to implement
-        a save and restore workflow
-        :param ResourceCommandContext context: the context object containing resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation.
-                Must be enabled in drivermetadata.xml as well
-        :param str mode: Snapshot save mode, can be one of two values 'shallow' (default) or 'deep'
-        :param str custom_params: Set of custom parameters for the save operation
-        :return: SavedResults serialized as JSON
-        :rtype: OrchestrationSaveResult
-        """
-
-        # See below an example implementation, here we use jsonpickle for serialization,
-        # to use this sample, you'll need to add jsonpickle to your requirements.txt file
-        # The JSON schema is defined at:
-        # https://github.com/QualiSystems/sandbox_orchestration_standard/blob/master/save%20%26%20restore/saved_artifact_info.schema.json
-        # You can find more information and examples examples in the spec document at
-        # https://github.com/QualiSystems/sandbox_orchestration_standard/blob/master/save%20%26%20restore/save%20%26%20restore%20standard.md
-        '''
-              # By convention, all dates should be UTC
-              created_date = datetime.datetime.utcnow()
-  
-              # This can be any unique identifier which can later be used to retrieve the artifact
-              # such as filepath etc.
-  
-              # By convention, all dates should be UTC
-              created_date = datetime.datetime.utcnow()
-  
-              # This can be any unique identifier which can later be used to retrieve the artifact
-              # such as filepath etc.
-              identifier = created_date.strftime('%y_%m_%d %H_%M_%S_%f')
-  
-              orchestration_saved_artifact = OrchestrationSavedArtifact('REPLACE_WITH_ARTIFACT_TYPE', identifier)
-  
-              saved_artifacts_info = OrchestrationSavedArtifactInfo(
-                  resource_name="some_resource",
-                  created_date=created_date,
-                  restore_rules=OrchestrationRestoreRules(requires_same_resource=True),
-                  saved_artifact=orchestration_saved_artifact)
-  
-              return OrchestrationSaveResult(saved_artifacts_info)
-        '''
-    pass
-
-    def orchestration_restore(self, context, cancellation_context, saved_artifact_info, custom_params):
-        """
-        Restores a saved artifact previously saved by this Shell driver using the orchestration_save function
-        :param ResourceCommandContext context: The context object for the command with resource and reservation info
-        :param CancellationContext cancellation_context: Object to signal a request for cancellation.
-                Must be enabled in drivermetadata.xml as well
-        :param str saved_artifact_info:
-                A JSON string representing the state to restore including saved artifacts and info
-        :param str custom_params: Set of custom parameters for the restore operation
-        :return: None
-        """
-        '''
-        # The saved_details JSON will be defined according to the JSON Schema and is the same object returned via the
-        # orchestration save function.
-        # Example input:
-        # {
-        #     "saved_artifact": {
-        #      "artifact_type": "REPLACE_WITH_ARTIFACT_TYPE",
-        #      "identifier": "16_08_09 11_21_35_657000"
-        #     },
-        #     "resource_name": "some_resource",
-        #     "restore_rules": {
-        #      "requires_same_resource": true
-        #     },
-        #     "created_date": "2016-08-09T11:21:35.657000"
-        #    }
-
-        # The example code below just parses and prints the saved artifact identifier
-        saved_details_object = json.loads(saved_details)
-        return saved_details_object[u'saved_artifact'][u'identifier']
-        '''
-        pass
     # </editor-fold>
