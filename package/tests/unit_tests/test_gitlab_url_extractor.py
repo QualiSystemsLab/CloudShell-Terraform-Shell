e19b243999cf2a8179621b648d5967d2c7717e19
katzy687 <katzy687@gmail.com>
2023-01-31 12:21:04 +0200

    rename test extractor class attribute

--- package/tests/unit_tests/test_gitlab_url_extractor.py
+++ package/tests/unit_tests/test_gitlab_url_extractor.py
@@ -3,11 +3,11 @@ from cloudshell.iac.terraform.downloaders import gitlab_downloader
 
 
 class TestGitlabUrlExtractor(unittest.TestCase):
-    NATURAL_URL = "http://192.168.85.26/quali_natti/terraformstuff/-/tree/test-branch/rds"
+    RAW_URL = "http://192.168.85.26/quali_natti/terraformstuff/-/tree/test-branch/rds"
     API_URL = "http://192.168.85.26/api/v4/projects/2/repository/archive.zip?path=rds"
 
     def test_raw_url(self):
-        url_data = gitlab_downloader.extract_data_from_raw_url(self.NATURAL_URL)
+        url_data = gitlab_downloader.extract_data_from_raw_url(self.RAW_URL)
         assert url_data
 
     def test_api_url_extract(self):
@@ -16,7 +16,7 @@ class TestGitlabUrlExtractor(unittest.TestCase):
 
     def test_api_validate(self):
         assert gitlab_downloader.is_gitlab_api_url(self.API_URL)
-        assert not gitlab_downloader.is_gitlab_api_url(self.NATURAL_URL)
+        assert not gitlab_downloader.is_gitlab_api_url(self.RAW_URL)
 
     def test_raises(self):
         url_arg = ""
